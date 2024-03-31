fe75ffdcf3d52c01e49f7f3db7074af09d205fb3
Shrikant Alone <shrikantalone66@gmail.com>
2024-03-31 14:39:47 +0530

    This is first commit by shrikant

---
+++ DoctorLogin.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.SqlClient;
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    } 
+    protected void btnsubmit_Click(object sender, EventArgs e)
+    {
+       
+
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Server=localhost;Initial catalog=DERMOCARE;Integrated security=true";
+        con.Open();
+
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "Select dname from DoctorMaster where username=@un and password=@pwd";
+        cmd.Parameters.AddWithValue("@un", txtusername.Text);
+        cmd.Parameters.AddWithValue("@pwd", txtpwd.Text);
+        cmd.Connection = con;
+        Object obj = cmd.ExecuteScalar();
+        con.Close();
+
+        if (obj == null)
+        {
+            lblmsg.Text = "Invalid Username or Password";
+        }
+        else
+        {
+            Session.Add("un", txtusername.Text);
+            Response.Redirect("Doctor\\Default.aspx");
+
+        }
+
+
+    }
+    }
---
+++ PatientLogin.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI; 
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.SqlClient;
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnsubmit_Click(object sender, EventArgs e)
+    {
+
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Server=DESKTOP-AJ9CFSH; Initial Catalog=DERMOCARE; Integrated Security=true ";
+        con.Open();
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "Select adhar from PatientMaster where  username= @un AND password=@pwd ";
+
+        cmd.Parameters.AddWithValue("@un", txtusername.Text);
+        cmd.Parameters.AddWithValue("@pwd", txtpwd.Text);
+
+        cmd.Connection = con;
+        Object obj = cmd.ExecuteScalar();
+
+
+        if (obj == null)
+        {
+            lblmsg.Text = "Invalid user";
+        }
+        else
+        {
+
+            {
+                Session.Add("u", txtusername.Text);
+                Session.Add("p", txtpwd.Text);
+                Response.Redirect("Patient//Default.aspx");
+            }
+            {
+
+
+            }
+            con.Close();
+
+
+
+
+
+        }
+    }
+}
